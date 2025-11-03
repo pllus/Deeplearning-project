deb029a931055c3fe25aecc9265c8fea92e624cd
Nitisarath <64013165+pllus@users.noreply.github.com>
2025-11-03 22:06:38 +0700

    Added Train Utils
    d

---
+++ config.py
@@ -0,0 +1,38 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+class config():
+  DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+  TRAIN_DIR = "/content/drive/MyDrive/Dataset/train"
+  VAL_DIR = "/content/drive/MyDrive/Dataset/val"
+  LEARNING_RATE = 2e-4
+  BATCH_SIZE = 4
+  NUM_WORKERS = 2
+  IMAGE_SIZE = 256
+  CHANNELS_IMG = 3
+  L1_LAMBDA = 100
+  LAMBDA_GP = 10
+  NUM_EPOCHS = 1
+  LOAD_MODEL = True
+  SAVE_MODEL = True
+  CHECKPOINT_DISC = "/content/drive/MyDrive/checkpoint/disc.pth.tar"
+  CHECKPOINT_GEN = "/content/drive/MyDrive/checkpoint/gen.pth.tar"
+  both_transform = A.Compose(
+    [A.Resize(width=256, height=256),], additional_targets={"image0": "image"},
+  )
+
+  transform_only_input = A.Compose(
+      [
+          A.HorizontalFlip(p=0.5),
+          A.ColorJitter(p=0.2),
+          A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+         ToTensorV2(),
+     ]
+  )
+
+  transform_only_mask = A.Compose(
+      [
+          A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+          ToTensorV2(),
+      ]
+  )
